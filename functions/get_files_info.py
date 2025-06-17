a61c4fa6fe2773436cbe5de01b490423683e7f97
HeigoTasuja <heigo.tasuja@gmail.com>
2025-06-17 22:25:53 +0300

    start working with file handling

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -24,3 +24,18 @@ def get_files_info(working_directory, directory=None):
         return "\n".join(files_info)
     except Exception as e:
         return f"Error listing files: {e}"
+
+
+def get_file_content(working_directory, file_path):
+    wrk_dir_abspath = os .path.abspath(working_directory)
+    work_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not work_file_path.startswith(wrk_dir_abspath):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(work_file_path):
+        return f'Error: File not found or is not a regular file "{file_path}"'
+
+    try:
+        with open(work_file_path, "r")
+        pass # :TODO finish this function
+
